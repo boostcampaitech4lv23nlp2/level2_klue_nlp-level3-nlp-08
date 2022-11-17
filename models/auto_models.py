fb2ea740620e7d5ce736e714965c8659829e796d
lhJoon <guswns971022@gmail.com>
2022-11-17 13:07:46 +0000

    add model for base

---
+++ models/auto_models.py
@@ -0,0 +1,15 @@
+import torch
+from torch import nn
+from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments, RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer
+from load_data import *
+
+class RE_Model(nn.Module):
+    def __init__(self,MODEL_NAME:str):
+        super(RE_Model).__init__()
+        self.MODEL_NAME = MODEL_NAME
+        # plm 모델 설정
+        self.plm = AutoModelForSequenceClassification.from_pretrained(self.MODEL_NAME,num_labels = 30)
+    
+    def forward(self,batch):
+        logits = self.plm(**batch)
+        return logits
